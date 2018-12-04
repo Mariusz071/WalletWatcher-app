72d124373365f3db1adce6454192a50765bf0a7c
Mariusz Siewruk <siewruk.mariusz92@gmail.com>
2018-12-04 14:52:36 +0000

    module structure setup

--- js/app.js
+++ js/app.js
@@ -1,1 +1,35 @@
 import '../styles/index.scss';
+
+let dataController = (function() {
+    // private methods / variables
+    
+
+
+    // public methods / variables object
+    
+} )()
+
+
+let uiController = (function() {
+    
+
+})();
+
+
+let appController = (function (data, ui) {
+    //code
+    let addBtn = document.querySelector('.add__btn');
+    addBtn.addEventListener('click', function() {
+        
+        //1. get input data
+        //2. add item to budget controller
+        //3. Add new item to ui
+        //4. Calculate the budget
+        //5. Display the budget in UI
+    })
+
+})(dataController, uiController);
+
+
+
+
