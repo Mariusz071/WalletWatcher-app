f5d8d9aaf5cf8aac0852bfc4d8a30edd4d8b0ea8
Mariusz Siewruk <siewruk.mariusz92@gmail.com>
2018-12-05 16:10:44 +0000

    getting input values

--- js/app.js
+++ js/app.js
@@ -11,21 +11,52 @@ let dataController = (function() {
 
 
 let uiController = (function() {
+    //class names in case we have to change them
+    let DOMelements = {
+        inputType: '.add__type',
+        inputDescription: '.add__description',
+        inputValue: '.add__value',
+        addButton: '.add__btn'
+    }
     
-
+    //public methods / variables object
+    return {
+        getInput: function() {
+            return {
+                type: document.querySelector(DOMelements.inputType).value,
+                description: document.querySelector(DOMelements.inputDescription).value,
+                value: document.querySelector(DOMelements.inputValue).value
+            };
+        },
+        getDOMelements: function() {
+            return DOMelements;
+        }
+    };
 })();
 
-
 let appController = (function (data, ui) {
-    //code
-    let addBtn = document.querySelector('.add__btn');
-    addBtn.addEventListener('click', function() {
-        
+    let DOM = uiController.getDOMelements();
+
+    let addItem = function() {
         //1. get input data
+        let input = uiController.getInput();
+        console.log(input);
+
+
         //2. add item to budget controller
         //3. Add new item to ui
         //4. Calculate the budget
         //5. Display the budget in UI
+        
+    }
+
+    let addBtn = document.querySelector(DOM.addButton);
+    addBtn.addEventListener('click', addItem);
+
+    document.addEventListener('keypress', function (e) {
+        if(e.keyCode === 13) {
+            addItem();
+        }
     })
 
 })(dataController, uiController);
