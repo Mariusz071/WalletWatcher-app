3eeaeeec7f9ed5fb237bcfb846aa49b7bf842f46
Mariusz Siewruk <siewruk.mariusz92@gmail.com>
2018-12-06 20:28:42 +0000

    adding input values into data structure

--- js/app.js
+++ js/app.js
@@ -1,14 +1,62 @@
 import '../styles/index.scss';
 
 let dataController = (function() {
-    // private methods / variables
-    
+
+    class Expense {
+        constructor(id, description, value) {
+            this.id = id,
+            this.description = description,
+            this.value = value
+        }
+    }
+
+    class Income {
+        constructor(id, description, value) {
+            this.id = id,
+            this.description = description,
+            this.value = value
+        }
+    }
 
 
-    // public methods / variables object
-    
+    let data = {
+        allValues: {
+            expense: [],
+            income: []
+        },
+        totalValues: {
+            expense: 0,
+            income: 0
+        }
+    }
+
+    return  {
+        addItem: function(type, descr, val) {
+            let newItem, ID;
+
+            //create new ID, check if array is empty
+            if (data.allValues[type].length > 0) {
+                ID = data.allValues[type][data.allValues[type].length - 1].id + 1;
+            } else ID = 0;
+            
+            //create new list entry based on the type input
+            if( type === 'expense' ) {
+                newItem = new Expense(ID, descr, val)
+            } else if ( type === 'income' )  {
+                newItem = new Income(ID, descr, val);
+            }
+            
+            //push new created item to the datas tructure
+            data.allValues[type].push(newItem);
+
+            // return newItem element
+            return newItem;
+        },
+        testData: function () {
+            console.log(data);
+        }
+    }
 } )()
-
 
 let uiController = (function() {
     //class names in case we have to change them
@@ -25,7 +73,7 @@ let uiController = (function() {
             return {
                 type: document.querySelector(DOMelements.inputType).value,
                 description: document.querySelector(DOMelements.inputDescription).value,
-                value: document.querySelector(DOMelements.inputValue).value
+                value: document.querySelector(DOMelements.inputValue).value,
             };
         },
         getDOMelements: function() {
@@ -48,11 +96,14 @@ let appController = (function (data, ui) {
     }
 
     let addItem = function() {
+        let input, newItem;
         //1. get input data
-        let input = uiController.getInput();
-        console.log(input);
+        input = uiController.getInput();
+        data.testData();
 
         //2. add item to budget controller
+        newItem = dataController.addItem(input.type, input.description, input.value);
+        console.log(newItem);
         //3. Add new item to ui
         //4. Calculate the budget
         //5. Display the budget in UI
