e717c448d65b9f3454c0b1c7facc6d4863403430
Mariusz Siewruk <siewruk.mariusz92@gmail.com>
2018-12-08 10:41:01 +0000

    adding list elements to the DOM

--- js/app.js
+++ js/app.js
@@ -64,7 +64,9 @@ let uiController = (function() {
         inputType: '.add__type',
         inputDescription: '.add__description',
         inputValue: '.add__value',
-        addButton: '.add__btn'
+        addButton: '.add__btn',
+        incomeContainer: '.income__list',
+        expensesContainer: '.expenses__list'
     }
     
     //public methods / variables object
@@ -78,7 +80,49 @@ let uiController = (function() {
         },
         getDOMelements: function() {
             return DOMelements;
-        }
+        },
+
+        addListItem: function(object, type) {
+            let html, newHtml, listElement;
+            // create html string with placeholders
+            if( type === 'income') {
+                listElement = DOMelements.incomeContainer;
+                html = `<div class="item clearfix" id="income-%id%">
+                            <div class="item__description">%description%</div>
+                            <div class="right clearfix">
+                                <div class="item__value">%value%</div>
+                                <div class="item__delete">
+                                    <button class="item__delete--btn">
+                                        <i class="ion-ios-close-outline"></i>
+                                    </button>
+                                </div>
+                            </div>
+                        </div>`
+            } else if ( type === 'expense' ) {
+                listElement = DOMelements.expensesContainer;
+                html = `<div class="item clearfix" id="expense-%id%">
+                            <div class="item__description">%description%</div>
+                            <div class="right clearfix">
+                                <div class="item__value">%value%</div>
+                                <div class="item__percentage">%50%</div>
+                                <div class="item__delete">
+                                    <button class="item__delete--btn">
+                                        <i class="ion-ios-close-outline"></i>
+                                    </button>
+                                </div>
+                            </div>
+                        </div>`
+            }
+            
+
+            // replace placeholders with the data
+            newHtml = html.replace('%id%', object.id);
+            newHtml = newHtml.replace('%description%', object.description);
+            newHtml = newHtml.replace('%value%', object.value);
+
+            // insert html into the DOM (as last child element of the list container)
+            document.querySelector(listElement).insertAdjacentHTML('beforeend', newHtml);
+        },
     };
 })();
 
@@ -104,7 +148,10 @@ let appController = (function (data, ui) {
         //2. add item to budget controller
         newItem = dataController.addItem(input.type, input.description, input.value);
         console.log(newItem);
+
         //3. Add new item to ui
+        uiController.addListItem(newItem, input.type);
+
         //4. Calculate the budget
         //5. Display the budget in UI
     };
